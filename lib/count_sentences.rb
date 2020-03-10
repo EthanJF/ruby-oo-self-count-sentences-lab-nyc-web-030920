66a566fcfb9b8356de524848ad67a38000aa56c7
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-10 21:19:17 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
 require 'pry'
 
 class String
-
+  
+  def last_character
+    self.split("").last
+  end
+  
   def sentence?
-    
+    self.last_character == "."
   end
 
   def question?
-
+    self.last_character == "?"
   end
 
   def exclamation?
-
+    self.last_character == "!"
   end
 
   def count_sentences
-
+    counter 
   end
 end
